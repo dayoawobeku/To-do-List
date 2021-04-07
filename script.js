4a46a754c8337650c5d442adea3da27e1738fef9
Dayo Awobeku <dawobeku@gmail.com>
2021-04-07 12:06:22 +0100

    Third Commit

--- script.js
+++ script.js
@@ -2,47 +2,35 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 var list = document.querySelectorAll("li");
-var li = document.createElement("li");
 
 function inputLength() {
   return input.value.length;
 }
-
-// for (var i = 0; i < list.length; i++) {
-//   list[i].addEventListener("click", liClick);
-// }
 
 function liClick() {
   this.classList.toggle("done");
 }
 
 function createListElement() {
-  li.appendChild(document.createTextNode(input.value));
-  // var btn = document.createElement("button");
-  // // btn.style.margin = "4px";
-  // btn.appendChild(document.createTextNode("delete"));
-  // btn = `${btn} + ${li.appendChild(btn)}`;
-  // li.addEventListener("click", function () {
-  //   var finished = this.classList.toggle("done");
-  // });
+  var li = document.createElement("li");
   ul.appendChild(li);
+  var text = document.createElement("span");
+  text.appendChild(document.createTextNode(input.value));
+  text.classList.add("strike");
   input.value = "";
+  li.appendChild(text);
+  var delBtn = document.createElement("button");
+  delBtn.appendChild(document.createTextNode("Delete"));
+  ul.appendChild(li).appendChild(delBtn);
+  delBtn.classList.add("delete");
+  ul.addEventListener("click", function (e) {
+    if (e.target.classList.contains("delete")) {
+      console.log(e.target.parentNode);
+      e.target.parentNode.remove();
+    }
+  });
+  li.addEventListener("click", liClick);
 }
-
-function attachDelBtn() {
-  const el = Array.from(ul.children);
-  console.log(el);
-  const html = el
-    .map((data) => {
-      return data.localName;
-    })
-    .join("");
-  console.log(html);
-  // console.log(typeof html);
-  // li.innerHTML = html;
-}
-
-attachDelBtn();
 
 function addListAfterClick() {
   if (inputLength() > 0) {
@@ -59,8 +47,3 @@ function addListAfterKeypress(event) {
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
-
-const image = document.querySelector(".image");
-image.addEventListener("click", (e) => {
-  console.log("I was clicked");
-});
